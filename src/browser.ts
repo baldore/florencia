ac01121b0dec2fe3ea2784f998238125afe76021
baldore <acornssn@gmail.com>
2018-01-15 16:59:25 -0500

    Move browser utilities to a new file

---
+++ src/browser.ts
@@ -0,0 +1,47 @@
+import * as puppeteer from 'puppeteer'
+
+export async function scrapImagesFrom(link: string) {
+  const browser = await puppeteer.launch({
+    // headless: false,
+    // devtools: true,
+  })
+  const page = await browser.newPage()
+
+  await page.goto(link)
+  await page.content()
+
+  await page.evaluate(async function() {
+    await new Promise((resolve, reject) => {
+      const container = document.querySelector('.document_scroller')
+      let currentScroll = 0
+
+      if (!container) {
+        return reject()
+      }
+
+      const scrollInterval = window.setInterval(() => {
+        currentScroll += container.clientHeight / 2
+        currentScroll = Math.min(container.scrollHeight, currentScroll)
+        container.scrollTop += currentScroll
+
+        if (currentScroll === container.scrollHeight) {
+          window.clearInterval(scrollInterval)
+          resolve()
+        }
+      }, 500)
+    })
+  })
+
+  console.log('Looking for images inside the page')
+  await page.waitFor(10000)
+
+  const images = await page.evaluate(() => {
+    const elements = Array.from(document.querySelectorAll('.absimg'))
+    const images = elements.map(img => img.getAttribute('src'))
+    return images
+  })
+
+  await browser.close()
+
+  return images
+}
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,13 @@
-import * as puppeteer from 'puppeteer'
+import { scrapImagesFrom } from './browser'
 
 // TODO: Add this link via CLI
 const link =
   'https://www.scribd.com/doc/50073955/Gentil-Montana-Obras-Para-Guitarra'
-const timeout = 30000
-
-async function getImages(link: string) {
-  const browser = await puppeteer.launch({
-    // headless: false,
-    // devtools: true,
-  })
-  const page = await browser.newPage()
-
-  await page.goto(link)
-  await page.content()
-
-  await page.evaluate(async function() {
-    await new Promise((resolve, reject) => {
-      const container = document.querySelector('.document_scroller')
-      let currentScroll = 0
-
-      if (!container) {
-        return reject()
-      }
-
-      const scrollInterval = window.setInterval(() => {
-        currentScroll += container.clientHeight / 2
-        currentScroll = Math.min(container.scrollHeight, currentScroll)
-        container.scrollTop += currentScroll
-
-        if (currentScroll === container.scrollHeight) {
-          window.clearInterval(scrollInterval)
-          resolve()
-        }
-      }, 500)
-    })
-  })
-
-  console.log('Looking for images inside the page')
-  await page.waitFor(10000)
-
-  const images = await page.evaluate(() => {
-    const elements = Array.from(document.querySelectorAll('.absimg'))
-    const images = elements.map(img => img.getAttribute('src'))
-    return images
-  })
-
-  await browser.close()
-
-  return images
-}
 
 async function main() {
   try {
     console.log('Starting scrap')
-    const images = await getImages(link)
+    const images = await scrapImagesFrom(link)
     console.log('wiiii', images)
   } catch (e) {
     console.log(e.message)
